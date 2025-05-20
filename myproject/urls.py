91db43892304be22a23e09c43d2c15ecb2dc7450
airlegend111 <carlosandres.baena.56@gmail.com>
2025-05-19 23:49:26 -0500

    Mejoras visuales en Pagina inicial

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
