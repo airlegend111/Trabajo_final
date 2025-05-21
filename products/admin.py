d9e34f58bd092a5418a73408c3d2cecdbfd1a095
GeroLGN <ggaviriacastaneda@gmail.com>
2025-05-21 02:09:01 -0500

    Operaciones Crud Completa

--- products/admin.py
+++ products/admin.py
@@ -1,43 +1,54 @@
 from django.contrib import admin
-from .models import Pizza
-from django.utils.html import format_html
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import GroupAdmin
+from .models import Product, Pizza, Cart, CartItem
+from users.models import Comment
+from users.decorators import restricted_admin_required
+from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
+from django.core.exceptions import PermissionDenied
+from django.utils import timezone
 
 class RestrictedPizzaAdmin(admin.ModelAdmin):
-    list_display = ('preview_image', 'name', 'size', 'price', 'created_at', 'actions_column')
-    list_filter = ('size', 'created_at')
+    list_display = ('name', 'size', 'price', 'created_by', 'modified_by', 'modified_at')
+    list_filter = ('size', 'created_at', 'modified_at')
     search_fields = ('name', 'description')
-    ordering = ('-created_at',)
+    ordering = ('-modified_at',)
     
     fieldsets = (
-        ('Información de la Pizza', {
-            'fields': ('name', 'description', 'size', 'price')
+        ('Información básica', {
+            'fields': ('name', 'description', 'size', 'price', 'image')
         }),
-        ('Imagen', {
-            'fields': ('image',),
-            'description': 'URL de la imagen de la pizza'
+        ('Información de auditoría', {
+            'fields': ('created_by', 'created_at', 'modified_by', 'modified_at'),
+            'classes': ('collapse',),
+            'description': 'Información sobre la creación y modificación de la pizza'
         })
     )
     
-    def preview_image(self, obj):
-        if obj.image:
-            return format_html('<img src="{}" style="width: 50px; height: 50px; border-radius: 5px;">', obj.image)
-        return "Sin imagen"
-    preview_image.short_description = 'Vista previa'
+    readonly_fields = ('created_by', 'created_at', 'modified_by', 'modified_at')
     
-    def actions_column(self, obj):
-        return format_html(
-            '<a class="button" href="{}">Editar</a>&nbsp;'
-            '<a class="button" style="background-color: #dc3545; color: white;" href="{}">Eliminar</a>',
-            f'/admin/products/pizza/{obj.id}/change/',
-            f'/admin/products/pizza/{obj.id}/delete/'
-        )
-    actions_column.short_description = 'Acciones'
+    def save_model(self, request, obj, form, change):
+        if not change:  # Si es una nueva pizza
+            obj.created_by = request.user
+        obj.modified_by = request.user
+        super().save_model(request, obj, form, change)
     
-    class Media:
-        css = {
-            'all': ('css/admin_styles.css',)
-        }
-
+    def has_module_permission(self, request):
+        return request.user.groups.filter(name='RestrictedAdmin').exists()
+    
+    def has_add_permission(self, request):
+        return request.user.groups.filter(name='RestrictedAdmin').exists()
+    
+    def has_change_permission(self, request, obj=None):
+        return request.user.groups.filter(name='RestrictedAdmin').exists()
+    
+    def has_delete_permission(self, request, obj=None):
+        return request.user.groups.filter(name='RestrictedAdmin').exists()
+    
+    def has_view_permission(self, request, obj=None):
+        return request.user.groups.filter(name='RestrictedAdmin').exists()
+    
 class RestrictedCommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'created_at', 'content')
     list_filter = ('created_at',)
